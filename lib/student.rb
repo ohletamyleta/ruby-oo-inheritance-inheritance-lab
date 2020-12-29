8a0d5d270d0b0f56242242d6afeac15387db2a89
ohletamyleta <ohletamyleta@gmail.com>
2020-12-29 17:22:54 -0500

    commit tree 75448be2aeb061393e5fad90d097a3910c5051d6

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
 
+
+    def initialize
+       @knowledge = []
+    
+    end
+
+    def learn(knowledge)
+      @knowledge << "Ruby framework Rails gem bundle update"
+
+    end
+
+    def knowledge
+        @knowledge << "Javascript Ember Elixir knowledge"
+    end 
+    
 end
